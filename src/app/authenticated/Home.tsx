6425e42e1a63b9a94d1746e0c6cf99adcaebbaf9
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-09-26 12:56:56 +0530

    home page re-skinned

--- src/app/authenticated/Home.tsx
+++ src/app/authenticated/Home.tsx
@@ -10,6 +10,7 @@ import { RootStackParamList } from '@/shared/RootStackedList';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import LanguageControl from '../components/LanguageControl'
+import { ArrowRight } from 'lucide-react-native';
 
 type LoginScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -24,60 +25,64 @@ const ERP: React.FC<Props> = ({ navigation }) => {
   const { t } = useTranslation();
 
   return (
-    <SafeAreaView className="flex-1 items-center w-full pt-20">
-      <View className="flex-row justify-between items-center w-4/5 mb-5">
+    <SafeAreaView className="flex-1 bg-white gap-2">
+      {/* Header */}
+      <View className="flex-row justify-between items-center px-6 pt-10 pb-4">
         <View>
-          <Text className="text-xl font-bold">{t('welcome.title') || 'Welcome back!'}</Text>
-          <Text className="text-sm text-gray-400">
+          <Text className="font-inter font-semibold text-lg leading-8 tracking-[-0.006em]">{t('welcome.title') || 'Welcome back!'}</Text>
+          <Text className="font-inter font-normal text-xs leading-5 tracking-normal text-[#64748B]">
             {t('welcome.subtitle') || 'Here is a List of ERP Systems available to you.'}
           </Text>
         </View>
         <LanguageControl />
       </View>
-      <ScrollView contentContainerStyle={{ alignItems: 'center', margin: 10, width: '100%' }}>
-        <View className="flex-row w-[95%] justify-center">
-          <TouchableOpacity 
-            className="w-[45%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2" 
-            onPress={() => navigation.navigate('FormsList')}
-          >
-            <Text>ERP 1</Text>
-            <Text>15 {t('navigation.forms') || 'Forms'}</Text>
+
+      {/* Pending Forms Card */}
+      <View className="mx-6 mb-5 p-4 w-[356px] h-[76px] border border-[##E2E8F0] rounded-lg bg-[#FFFCF0] flex-row justify-between items-center">
+        <View>
+          <Text className="text-[#DC7609] text-2xl font-bold">15 Forms Pending</Text>
+          <TouchableOpacity>
+            <View className="flex-row items-center mt-2 gap-2">
+              <Text className="text-[#DC7609]">View Pending Forms</Text>
+              <ArrowRight color="#DC7609" size={12} />
+            </View>
           </TouchableOpacity>
-          <View className="w-[45%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2">
-            <Text>ERP 2</Text>
-            <Text>15 {t('navigation.forms') || 'Forms'}</Text>
-          </View>
         </View>
+        <TouchableOpacity>
+          <Text className="border border-[#DC7609] px-3 py-2 text-base text-[#DC7609] rounded-xl">
+            Submit all forms
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-        <View className="flex-row w-[95%] justify-center">
-          <View className="w-[45%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2">
-            <Text>ERP 3</Text>
-            <Text>15 {t('navigation.forms') || 'Forms'}</Text>
-          </View>
-          <View className="w-[45%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2">
-            <Text>ERP 4</Text>
-            <Text>15 {t('navigation.forms') || 'Forms'}</Text>
-          </View>
+      {/* ERP Systems */}
+      <ScrollView contentContainerStyle={{ paddingBottom: 30 }}>
+        <View className="flex-row flex-wrap justify-center">
+          {['ERP 1', 'ERP 2', 'ERP 3', 'ERP 4', 'ERP 5', 'ERP 6'].map((label, i) => (
+            <TouchableOpacity
+              key={i}
+              className="w-[173px] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2"
+              onPress={() => i === 0 && navigation.navigate('FormsList')}
+            >
+              <Text className="w-[42px] h-[28px] font-inter font-semibold text-base leading-7 tracking-[-0.006em]">
+                {label}
+              </Text>
+              <Text className='font-inter font-normal text-xs leading-5 tracking-normal text-center'>15 {t('navigation.forms') || 'Forms'}</Text>
+            </TouchableOpacity>
+          ))}
         </View>
-
-        <View className="flex-row w-[95%] justify-center">
-          <View className="w-[45%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2">
-            <Text>ERP 5</Text>
-            <Text>15 {t('navigation.forms') || 'Forms'}</Text>
-          </View>
-          <View className="w-[45%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2">
-            <Text>ERP 6</Text>
-            <Text>15 {t('navigation.forms') || 'Forms'}</Text>
-          </View>
-        </View>
+        {/* Downloads */}
         <TouchableOpacity
-          className="w-[95%] min-h-[100px] rounded-2xl border border-black items-center justify-center m-2 mt-5"
+          className="w-[35%] min-h-[100px] rounded-2xl border border-black items-center justify-center mx-auto mt-6"
           onPress={() => navigation.navigate('Downloads')}
         >
-          <Text className="text-base font-bold">{t('navigation.downloads') || 'Downloads'}</Text>
+          <Text className="font-inter font-semibold text-sm leading-6 tracking-[-0.006em] text-gray-800">
+            {t('navigation.downloads') || 'Downloads'}
+          </Text>
         </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
+
   );
 };
 
