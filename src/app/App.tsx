39b334568e530fd5976e8cb99ef8259c479efc8f
Maddi Jitendra <maddikittu7@gmail.com>
2025-10-09 14:59:08 +0530

    Add dotenv support for environment variables, include Google Sign-In pod, and update TypeScript configuration to include source files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import {
-  StatusBar,
-  useColorScheme,
-  SafeAreaView,
-} from 'react-native';
+import {StatusBar, useColorScheme, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { enableScreens } from 'react-native-screens';
+import {enableScreens} from 'react-native-screens';
 import Login from './public/Login';
 import ERP from './authenticated/Home';
 import FormsList from './authenticated/workflow/form/FormsList';
 import FormDetail from './authenticated/workflow/form/FormDetail';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { RootStackParamList } from '@/shared/RootStackedList';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {RootStackParamList} from '@/shared/RootStackedList';
 
 enableScreens();
-
-
-
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -36,12 +28,13 @@ function App(): React.JSX.Element {
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{headerShown: false}}>
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="ERP" component={ERP} />
           <Stack.Screen name="FormsList" component={FormsList} />
           <Stack.Screen name="FormDetail" component={FormDetail} />
-          
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
